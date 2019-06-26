803bb5e61a6a4f3eb8e728c3f0d080354fe3aa0c
Emanuel <emanuel.moldovan@movalio.com>
2019-06-26 14:51:02 +0300

    try 6

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
-// urlB64ToUint8Array is a magic function that will encode the base64 public key
-// to Array buffer which is needed by the subscription option
-const urlB64ToUint8Array = base64String => {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
-  const rawData = atob(base64)
-  const outputArray = new Uint8Array(rawData.length)
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i)
-  }
-  return outputArray
-}
-self.addEventListener('activate', async () => {
-  // This will be called only once when the service worker is activated.
-  try {
-    const applicationServerKey = urlB64ToUint8Array(
-      'BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk'
-    )
-    const options = { applicationServerKey, userVisibleOnly: true }
-    const subscription = await self.registration.pushManager.subscribe(options)
-    console.log(JSON.stringify(subscription))
-  } catch (err) {
-    console.log('Error', err)
-  }
-})
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBAR5OWsAsKpDGdv5Pkupaw9J40bS5OY5w",
+  authDomain: "push-client-website.firebaseapp.com",
+  databaseURL: "https://push-client-website.firebaseio.com",
+  projectId: "push-client-website",
+  storageBucket: "",
+  messagingSenderId: "893328877473",
+  appId: "1:893328877473:web:9432a642dced1ef2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
