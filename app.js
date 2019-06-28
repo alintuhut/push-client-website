203968d3f9a38ca74b844a44e2d5ef9fda3071ec
Emanuel <emanuel.moldovan@movalio.com>
2019-06-28 11:37:09 +0300

    fix app.js

--- app.js
+++ app.js
@@ -1,143 +1,152 @@
-function loadScript(url) {
-  return new Promise(resolve => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    script.onload = function() {
-      resolve(true);
-    };
-    document.head.appendChild(script);
-  });
-}
+const App = (function() {
 
-async function loadDependencies() {
-  await loadScript('https://www.gstatic.com/firebasejs/6.2.3/firebase-app.js');
-  await loadScript(
-    'https://www.gstatic.com/firebasejs/6.2.3/firebase-messaging.js'
-  );
+  let messaging = null;
 
-  return true;
-}
-
-const initializeFirebase = () => {
-  var firebaseConfig = {
-    apiKey: 'AIzaSyBAR5OWsAsKpDGdv5Pkupaw9J40bS5OY5w',
-    authDomain: 'push-client-website.firebaseapp.com',
-    databaseURL: 'https://push-client-website.firebaseio.com',
-    projectId: 'push-client-website',
-    storageBucket: '',
-    messagingSenderId: '893328877473',
-    appId: '1:893328877473:web:9432a642dced1ef2',
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-};
-
-const askForPermissionToReceiveNotifications = async () => {
-  try {
-    const messaging = firebase.messaging();
-    const permission = await Notification.requestPermission();
-
-    navigator.permissions.query({name:'notifications'}).then(function(permissionStatus) {
-      console.log('notifications permission state is ', permissionStatus.state);
-      permissionStatus.onchange = function() {
-        console.log('notifications permission state has changed to ', this.state);
+  function loadScript(url) {
+    return new Promise(resolve => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+      script.onload = function() {
+        resolve(true);
       };
+      document.head.appendChild(script);
     });
-
-    console.log('Event notification prompt', permission);
-
-    const token = await messaging.getToken();
-    console.log('token is:', token);
-
-    return token;
-  } catch (error) {
-    console.error(error);
   }
-};
 
-const registerServiceWorker = async () => {
-  try {
-    //const registration = await navigator.serviceWorker.register('sw.js');
-
-    //console.log('Service worker successfully registered.', registration);
-    askForPermissionToReceiveNotifications();
-    const messaging = firebase.messaging();
-    //messaging.useServiceWorker(registration);
-    messaging.usePublicVapidKey(
-      'BCPI-kjlPHSPpzH15AsX0YXwkHTp6lYK20W51TG1LTdJTJ_xnyr70fRBqNZFTaai8_6NkUqB8jkYed9ZX1mu0C8'
+  async function loadDependencies() {
+    await loadScript('https://www.gstatic.com/firebasejs/6.2.3/firebase-app.js');
+    await loadScript(
+      'https://www.gstatic.com/firebasejs/6.2.3/firebase-messaging.js'
     );
 
-    messaging.onMessage(payload => {
-      console.log('Message received. ', payload);
-      navigator.serviceWorker.ready.then(registration => {
-        showLocalNotification('hello', payload.data.message, registration);
+    return true;
+  }
+
+  function initializeFirebase() {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyBAR5OWsAsKpDGdv5Pkupaw9J40bS5OY5w',
+      authDomain: 'push-client-website.firebaseapp.com',
+      databaseURL: 'https://push-client-website.firebaseio.com',
+      projectId: 'push-client-website',
+      storageBucket: '',
+      messagingSenderId: '893328877473',
+      appId: '1:893328877473:web:9432a642dced1ef2',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    messaging = firebase.messaging();
+  };
+
+  async function askForPermissionToReceiveNotifications () {
+    try {
+      const permission = await Notification.requestPermission();
+
+      navigator.permissions.query({name:'notifications'}).then(function(permissionStatus) {
+        console.log('notifications permission state is ', permissionStatus.state);
+        permissionStatus.onchange = function() {
+          console.log('notifications permission state has changed to ', this.state);
+        };
       });
-    });
 
-    messaging.onTokenRefresh(() => {
-      try {
-        const refreshedToken = messaging.getToken();
-        console.log('Token refreshed.', refreshedToken);
-      } catch (error) {
-        console.log('Unable to retrieve refreshed token ', error);
+      console.log('Event notification prompt', permission);
+
+      const token = await messaging.getToken();
+      console.log('token is:', token);
+
+      return token;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  async function registerServiceWorker () {
+    try {
+      const registration = await navigator.serviceWorker.register('sw.js');
+
+      console.log('Service worker successfully registered.', registration);
+      askForPermissionToReceiveNotifications();
+
+      messaging.useServiceWorker(registration);
+      messaging.usePublicVapidKey(
+        'BCPI-kjlPHSPpzH15AsX0YXwkHTp6lYK20W51TG1LTdJTJ_xnyr70fRBqNZFTaai8_6NkUqB8jkYed9ZX1mu0C8'
+      );
+
+      messaging.onMessage(payload => {
+        console.log('Message received. ', payload);
+        navigator.serviceWorker.ready.then(registration => {
+          showLocalNotification('hello', payload.data.message, registration);
+        });
+      });
+
+      messaging.onTokenRefresh(() => {
+        try {
+          const refreshedToken = messaging.getToken();
+          console.log('Token refreshed.', refreshedToken);
+        } catch (error) {
+          console.log('Unable to retrieve refreshed token ', error);
+        }
+      });
+
+    } catch (error) {
+      console.error('Unable to register service worker.', error);
+    }
+  };
+
+  function check () {
+    if (!('serviceWorker' in navigator)) {
+      throw new Error('No Service Worker support!');
+    }
+    if (!('PushManager' in window)) {
+      throw new Error('No Push API Support!');
+    }
+
+    const channel = new BroadcastChannel('app-channel');
+    channel.onmessage = function(e) {
+      console.log('In app.js:', e.data);
+    };
+
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+      // Let's see if you have a subscription already
+      return serviceWorkerRegistration.pushManager.getSubscription();
+    })
+    .then(function(subscription) {
+      if (!subscription) {
+        // You do not have subscription
       }
-    });
+      // You have subscription.
+      // Send data to service worker
+      const messageChannel = new MessageChannel();
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage('hello from app.js', [messageChannel.port2]);
+      }
 
-  } catch (error) {
-    console.error('Unable to register service worker.', error);
-  }
-};
-
-const check = () => {
-  if (!('serviceWorker' in navigator)) {
-    throw new Error('No Service Worker support!');
-  }
-  if (!('PushManager' in window)) {
-    throw new Error('No Push API Support!');
-  }
-
-  const channel = new BroadcastChannel('app-channel');
-  channel.onmessage = function(e) {
-    console.log('In app.js:', e.data);
+    })
   };
 
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    // Let's see if you have a subscription already
-    return serviceWorkerRegistration.pushManager.getSubscription();
-  })
-  .then(function(subscription) {
-    if (!subscription) {
-      // You do not have subscription
-    }
-    // You have subscription.
-    // Send data to service worker
-    const messageChannel = new MessageChannel();
-    if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage('hello from app.js', [messageChannel.port2]);
-    }
-
-  })
-};
-
-const showLocalNotification = (title, body, swRegistration) => {
-  const options = {
-    body,
-    tag: 'request',
-    actions: [{ action: 'yes', title: 'Yes' }, { action: 'no', title: 'No' }],
+  function showLocalNotification (title, body, swRegistration) {
+    const options = {
+      body,
+      tag: 'request',
+      actions: [{ action: 'yes', title: 'Yes' }, { action: 'no', title: 'No' }],
+    };
+    swRegistration.showNotification(title, options);
   };
-  swRegistration.showNotification(title, options);
-};
 
-const askForPermission = async () => {
-  check();
-  askForPermissionToReceiveNotifications();
-};
+  return {
 
-const init = async () => {
-  await loadDependencies();
-  initializeFirebase();
-  registerServiceWorker();
-};
+    init() {
+      await loadDependencies();
+      initializeFirebase();
+      registerServiceWorker();
+    },
 
-init();
+    askForPermission () {
+      check();
+      askForPermissionToReceiveNotifications();
+    },
+
+  };
+})();
+
+App.init();
