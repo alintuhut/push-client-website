779cd744b68bc3c0aa5754b9adeb47c7fcb8666c
Emanuel <emanuel.moldovan@movalio.com>
2019-07-02 11:36:31 +0300

    Proper event calls from client website script + load script from remote url (2) [

--- sw.js
+++ sw.js
@@ -37,25 +37,31 @@ messaging.setBackgroundMessageHandler(function(payload) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  postData('https://app.movalio.com', {action: 'notification:click', app_id: '4455', token: '123123' })
-  .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
-  .catch(error => console.error(error));
+  postData('https://app.movalio.com', {action: 'notification:click', app_id: '4455', token: '123123' });
 });
 
-function postData(url = '', data = {}) {
-  // Default options are marked with *
-    return fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, cors, *same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    })
-    .then(response => response.json()); // parses JSON response into native JavaScript objects
-}
+// Set up our HTTP request
+var xhr = new XMLHttpRequest();
+
+// Setup our listener to process completed requests
+xhr.onload = function () {
+
+	// Process our return data
+	if (xhr.status >= 200 && xhr.status < 300) {
+		// This will run when the request is successful
+		console.log('success!', xhr);
+	} else {
+		// This will run when it's not
+		console.log('The request failed!');
+	}
+
+	// This will run either way
+	// All three of these are optional, depending on what you're trying to do
+	console.log('This always runs...');
+};
+
+const postData = (url, body) => {
+  xhr.open('POST', ur);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(body);
+};
